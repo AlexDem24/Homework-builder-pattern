627c70f10641a4c90da6af4aa740cdb600a40087
AlexDem24 <alexdemidov000@mail.ru>
2022-05-21 11:49:52 +0300

    Initial commit

---
+++ src/main/java/PersonBuilder.java
@@ -0,0 +1,44 @@
+public class PersonBuilder {
+
+    protected String name;
+    protected String surname;
+    protected int age;
+    protected String address;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        if (age <= 0) {
+            throw new IllegalStateException("Указан некорректный возраст");
+        }
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() {
+        Person person;
+        if (name == null || surname == null) {
+            throw new IllegalStateException("Имя или фамилия не указаны");
+        }
+        if (age <= 0) {
+            person = new Person(name, surname);
+        } else {
+            person = new Person(name, surname, age);
+        }
+        person.setAddress(address);
+        return person;
+    }
+}
